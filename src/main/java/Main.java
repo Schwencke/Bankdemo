8c010de9d6b45948262a25509bfd3a288e1deeff
Thomas Overgaard <68824543+Schwencke@users.noreply.github.com>
2021-02-16 11:05:59 +0100

    cleanup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,24 +7,8 @@ import java.util.List;
 
 public class Main {
 
-
-
     public static void main(String[] args) {
 
-         final String USER = "bankuser";
-         final String PASSWORD = "Bank123";
-         final String URL = "jdbc:mysql://localhost:3306/bank?serverTimezone=CET&useSSL=false";
-
-         Database database = new Database(USER,PASSWORD,URL);
-         DbMapper dbMapper = new DbMapper(database);
-
-         List<Customer> customerList = dbMapper.viewAllCustomers();
-        for (Customer customer : customerList) {
-            System.out.println(customer.getCustomer_no());
-            System.out.println(customer.getFirst_name());
-            System.out.println(customer.getLast_name());
-
-        }
 
     }
 }
